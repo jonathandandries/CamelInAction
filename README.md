3c338a2ced0f4d7800190dc5d0540779a238ceb4
Jonathan D'Andries <jonathan_dandries@ao.uscourts.gov>
2020-03-07 11:37:51 -0500

    First working version

---
+++ README.md
@@ -0,0 +1,14 @@
+# leavetracking-api-rest-service
+
+Working endpoints:
+
+curl http://localhost:9090/LeaveTracking/api/v1/api-doc/swagger.yaml
+curl http://localhost:9090/LeaveTracking/api/v1/schedule
+
+References:
+
+ 1. https://access.redhat.com/documentation/en-us/red_hat_fuse/7.1/html/fuse_on_openshift_guide/camel-spring-boot
+ 2. Swagger auto: https://medium.com/@bszeti/swagger-with-spring-boot-and-camel-ac59cca9556e
+ 3. Swagger static: https://dzone.com/articles/spring-boot-apache-camel-and-swagger-ui-1
+ 4. Baeldung: https://www.baeldung.com/apache-camel-spring-boot
+
---
+++ rider-auto/src/main/java/jonathandandries/example/camelinaction/riderauto/SampleCamelRouter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jonathandandries.example.camelinaction.riderauto;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestConfigurationDefinition;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author jonathandandries
+ */
+@Component
+public class SampleCamelRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // If you do restConfiguration here, it will override the one in camel-context.xml.
+        // We should create do this in an @Configuration class so that it is only done once.
+        RestConfigurationDefinition contextPath = restConfiguration()
+                .component("servlet")
+                .bindingMode(RestBindingMode.json)
+                .contextPath("/RiderAuto/api/v1")
+                .apiContextPath("/api-doc");
+
+        rest().get("/hello")
+                .to("direct:hello");
+
+        from("direct:hello")
+                .log(LoggingLevel.INFO, "Hello World")
+                .transform().simple("Hello World");
+    }
+}
