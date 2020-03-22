f821fd56f4b63eaa39f8a818b8258b66cb505a1f
Jonathan D'Andries <jonathan_dandries@ao.uscourts.gov>
2020-03-22 13:27:22 -0400

    Implement BackendStub for consuming Order objects from JMS, and move data messages out of the placeorder subfolder

--- rider-auto/src/main/java/jonathandandries/example/camelinaction/riderauto/SampleCamelRouter.java
+++ rider-auto/src/main/java/jonathandandries/example/camelinaction/riderauto/SampleCamelRouter.java
@@ -5,12 +5,16 @@
  */
 package jonathandandries.example.camelinaction.riderauto;
 
+import java.util.Arrays;
 import javax.xml.bind.JAXBContext;
-import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import jonathandandries.example.camelinaction.riderauto.model.Order;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.model.dataformat.BindyType;
 import org.apache.camel.spi.DataFormat;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,13 +23,23 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SampleCamelRouter extends RouteBuilder {
-
+    
+    @Bean
+    JmsComponent jms() {
+        // Spring will automatically inject this Bean into CamelContext for us.
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("vm://localhost");
+        // Need to setTrustedPackagers for JMS because were failing to pull Order.class off the JMS queue because it's not a "Trusted Class".
+        factory.setTrustedPackages(Arrays.asList("jonathandandries.example.camelinaction.riderauto.model"));
+        return JmsComponent.jmsComponentAutoAcknowledge(factory);
+    }
+    
     @Override
     public void configure() throws Exception {
         DataFormat jaxbFormat = new JaxbDataFormat(JAXBContext.newInstance(jonathandandries.example.camelinaction.riderauto.model.Order.class));
 
-        from("file:data/placeorder?delete=true")
+        from("file:data/placeorder?delete=true&exclude=.gitkeep")
                 .routeId("FileToJMS")
+                .log("Body: ${body}")
                 .to("jms:incomingOrders")
                 .to("file:data");
 
@@ -44,21 +58,14 @@ public class SampleCamelRouter extends RouteBuilder {
                     .log("XML: ${body.name} | ${body.amount}")
                     .to("jms:orders")
                 .otherwise()
-                    .unmarshal().bindy(BindyType.Csv, jonathandandries.example.camelinaction.riderauto.model.Order.class)
+                    .unmarshal().bindy(BindyType.Csv, Order.class)
                     .log("CSV: ${body.name} | ${body.amount}")
                     .to("jms:orders");
-
-        // ToDo: Failing to pull Order.class off the JMS queue because it's not a "Trusted Class".
-//        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-//        factory.setTrustAllPackages(true);
-        
-        
-//        from("jms:orders")
-//                .routeId("BackendStub")
-//                .convertBodyTo(String.class)
-//                .log("${body}");
-//                .convertBodyTo(jonathandandries.example.camelinaction.riderauto.model.Order.class)
-//                .log("${body.name} | ${body.amount}");
+                
+        from("jms:orders")
+                .routeId("BackendStub")
+                .convertBodyTo(Order.class)
+                .log("Received order for ${body.amount} of ${body.name}");
 
     }
 }
